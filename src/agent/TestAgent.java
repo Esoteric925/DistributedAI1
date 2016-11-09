9e7d794637f7c5b9806fb48550b60f6c1154c984
Esoteric925 <haval925@gmail.com>
2016-11-09 14:35:46 +0100

    la upp allt

---
+++ src/agent/TestAgent.java
@@ -0,0 +1,19 @@
+package agent;
+
+import jade.core.Agent;
+
+/**
+ * Created by Amir and Araxi on 2016-11-09.
+ */
+public class TestAgent extends Agent {
+
+
+    protected void setup(){
+
+        System.out.println("Hello! Buyer-agent" + getAID().getName() + " is ready");
+
+    }
+
+
+
+}
