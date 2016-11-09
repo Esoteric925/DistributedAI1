1a6d0e7484bc6f27eeb4ae20f1a9560b8ab1c66a
Araxi Mekhitarian <araxii@live.se>
2016-11-09 13:14:44 +0100

    hahha1

--- src/TestAgent.java
+++ src/TestAgent.java
@@ -1,14 +1,14 @@
 import jade.core.Agent;
 
 /**
- * Created by Amir on 2016-11-09.
+ * Created by Amir and Araxi on 2016-11-09.
  */
 public class TestAgent extends Agent {
 
     public static void main(String[] args) {
 
 
-        System.out.println("Hej");
+        System.out.println("bajs");
         
 
     }
